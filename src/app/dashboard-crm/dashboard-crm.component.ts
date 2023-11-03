2a3658e5eb4404087e626ccea6a61ad5d867cd0d
element0355 <58042491+element0355@users.noreply.github.com>
2023-11-03 10:46:14 -0400

    Update dashboard-crm.component.ts

--- src/app/dashboard-crm/dashboard-crm.component.ts
+++ src/app/dashboard-crm/dashboard-crm.component.ts
@@ -137,9 +137,9 @@ export class DashboardCrmComponent implements OnInit {
           let current_delegate_total_vote_count2 = 0;
           let xcash_wallet_decimal_places_amount = this.httpdataservice.XCASH_WALLET_DECIMAL_PLACES_AMOUNT;
 
-          this.dashCard1[3].text = data2.length;
+          this.dashCard1[3].text = data2.currentBlockVerifiersValidAmount;
 
-          for (count = 0 ; count < data2.length; count++)
+          for (count = 0 ; count < data2.currentBlockVerifiersValidAmount; count++)
           {
             current_delegate_total_vote_count2 = parseInt(data2[count].totalVotes) / xcash_wallet_decimal_places_amount;
             delegate_total_vote_count += current_delegate_total_vote_count2;
