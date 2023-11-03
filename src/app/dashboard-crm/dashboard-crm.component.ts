05e04cfb73842c2dd4691402a5ab59e379a5f450
element0355 <58042491+element0355@users.noreply.github.com>
2023-11-03 11:35:14 -0400

    Update dashboard-crm.component.ts

--- src/app/dashboard-crm/dashboard-crm.component.ts
+++ src/app/dashboard-crm/dashboard-crm.component.ts
@@ -36,7 +36,7 @@ export class DashboardCrmComponent implements OnInit {
       { ogmeter: false,  width_icon: 25, text_size: 40, text: 0, suffix: '', title: 'AVERAGE DELEGATE TOTAL VOTE', icon: 'signal_cellular_null' },
       { ogmeter: true,  width_icon: 25, text_size: 40, text: 0, suffix: '%', title: 'PoS CIRCULATING', icon: 'pie_chart' },
       { ogmeter: true,  width_icon: 25, text_size: 40, text: 0, suffix: '', title: 'DPoPS ROUND NUMBER', icon: 'autorenew' },
-      { ogmeter: false,  width_icon: 25, text_size: 40, text: 0, suffix: '', title: 'RANK '+ environment.totalBlockVerifiers +' TOTAL VOTE ', icon: 'swap_vert' },
+      { ogmeter: false,  width_icon: 25, text_size: 40, text: 0, suffix: '', title: 'TOTAL VOTERS', icon: 'swap_vert' },
       { ogmeter: false,  width_icon: 25, text_size: 40, text: 0, suffix: '', title: 'BLOCK REWARD ', icon: 'toll' },
       { ogmeter: false,  width_icon: 25, text_size: 40, text: 0, suffix: '', title: 'BLOCK TIME ', icon: 'timelapse' },
       { ogmeter: false,  width_icon: 25, text_size: 40, text: 0, suffix: '', title: 'EST. BLOCK PER DAY ', icon: 'view_week' },
@@ -111,6 +111,7 @@ export class DashboardCrmComponent implements OnInit {
           this.dashCard1[4].text = this.functionsService.get_lg_numer_format(parseInt(data.totalVotes) / this.httpdataservice.XCASH_WALLET_DECIMAL_PLACES_AMOUNT);
           this.dashCard1[6].text = parseInt(data.votePercentage);
           this.dashCard1[5].text = this.functionsService.get_lg_numer_format(parseInt(data.averageVote) / this.httpdataservice.XCASH_WALLET_DECIMAL_PLACES_AMOUNT);
+          this.dashCard1[8].text = data.totalVoters;
         },
         (error) =>  {
           Swal.fire({
@@ -123,45 +124,4 @@ export class DashboardCrmComponent implements OnInit {
         }
       );
     }
-
-
-
-    get_delegates() {
-      // get the data
-      this.httpdataservice.get_request(this.httpdataservice.GET_STATISTICS).subscribe(
-        (res) => {
-
-          let data2 = JSON.parse(JSON.stringify(res));
-
-          let count = 0;
-          let delegate_total_vote_count = 0;
-          let current_delegate_total_vote_count2 = 0;
-          let xcash_wallet_decimal_places_amount = this.httpdataservice.XCASH_WALLET_DECIMAL_PLACES_AMOUNT;
-
-          this.dashCard1[3].text = data2.currentBlockVerifiersValidAmount;
-
-          for (count = 0 ; count < data2.totalVotes; count++)
-          {
-            current_delegate_total_vote_count2 = parseInt(data2[count].totalVotes) / xcash_wallet_decimal_places_amount;
-            delegate_total_vote_count += current_delegate_total_vote_count2;
-          }
-
-          this.dashCard1[8].text = this.functionsService.get_lg_numer_format(parseInt(data2[environment.totalBlockVerifiers - 1](data2.totalVotes)) / this.httpdataservice.XCASH_WALLET_DECIMAL_PLACES_AMOUNT );
-          // only use 45 to calculate this since there are no votes for the 5 seed nodes
-          //var avg_vote_count = this.functionsService.get_lg_numer_format(delegate_total_vote_count / environment.totalBlockVerifiers - 3);
-
-
-        },
-        (error) => {
-          Swal.fire({
-              title: "Error",
-              html: "An error has occured:<br>API: Get delegates failed.",
-              icon: "error",
-              position: 'bottom',
-              timer: 2500
-            });
-        }
-      );
-    }
-
 }
