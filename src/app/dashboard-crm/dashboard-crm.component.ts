b34c21e79c2652042e26e35dfc45d6387e54169d
element0355 <58042491+element0355@users.noreply.github.com>
2023-11-03 11:20:41 -0400

    Update dashboard-crm.component.ts

--- src/app/dashboard-crm/dashboard-crm.component.ts
+++ src/app/dashboard-crm/dashboard-crm.component.ts
@@ -145,7 +145,7 @@ export class DashboardCrmComponent implements OnInit {
             delegate_total_vote_count += current_delegate_total_vote_count2;
           }
 
-          this.dashCard1[8].text = this.functionsService.get_lg_numer_format(parseInt(data2[environment.totalBlockVerifiers - 1]data2.totalVotes) / this.httpdataservice.XCASH_WALLET_DECIMAL_PLACES_AMOUNT );
+          this.dashCard1[8].text = this.functionsService.get_lg_numer_format(parseInt(data2[environment.totalBlockVerifiers - 1](data2.totalVotes)) / this.httpdataservice.XCASH_WALLET_DECIMAL_PLACES_AMOUNT );
           // only use 45 to calculate this since there are no votes for the 5 seed nodes
           //var avg_vote_count = this.functionsService.get_lg_numer_format(delegate_total_vote_count / environment.totalBlockVerifiers - 3);
           this.dashCard1[5].text = this.functionsService.get_lg_numer_format(parseInt(data2.averageVote) / this.httpdataservice.XCASH_WALLET_DECIMAL_PLACES_AMOUNT); //avg_vote_count;
