8987693cfb11d5da941cf786e9f353b16a7593a8
element0355 <58042491+element0355@users.noreply.github.com>
2023-11-03 12:09:52 -0400

    Update delegates_statistics.component.ts

--- src/app/tables/delegates_statistics/delegates_statistics.component.ts
+++ src/app/tables/delegates_statistics/delegates_statistics.component.ts
@@ -80,7 +80,7 @@ export class Delegates_statisticsComponent implements OnInit {
 
           this.dashCard1[0].text = data.online == 'true' ? 'Online'  : 'Offline';
           this.dashCard1[1].text = parseInt(data.rank);
-          //this.dashCard1[2].text = block_producer_block_heights.length-1;
+          this.dashCard1[2].text = data.totalBlockProducerRounds;
           this.dashCard1[3].text = this.functionsService.get_lg_numer_format(parseInt(data.votes) / this.httpdataservice.XCASH_WALLET_DECIMAL_PLACES_AMOUNT);
 
           this.dashCard1[4].text = parseInt(data.onlinePercentage);
@@ -88,7 +88,7 @@ export class Delegates_statisticsComponent implements OnInit {
           this.dashCard1[6].text = parseInt(data.totalBlockProducerRounds);
           //this.dashCard1[7].text = parseInt(data.block_verifier_score);
 
-          this.dashCard1[8].text = parseInt(data.totalRounds) / parseInt(data.totalBlockProducerRounds) * 100;
+          this.dashCard1[8].text = parseInt(data.totalBlockProducerRounds) / parseInt(data.totalRounds) * 100;
           this.dashCard1[9].text = data.totalRounds > 0 ? parseInt(data.totalBlockProducerRounds) / parseInt(data.totalRounds) : "0";
           this.dashCard1[11].text = data.fee == '' ? '0' : data.fee;
 
